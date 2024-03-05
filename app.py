de3a9a13c39331ba9e2457058e9ab40298f01088
DianNurrahmad <diannurrahmad03@gmail.com>
2024-03-05 10:33:26 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,7 @@
+def luas_segitiga():
+    a = int (input("Masukkan alas segitiga "))
+    t = int (input("Masukkan tinggi segitiga "))
+    luas = a*t/2
+    print ("Luas segitiga adalah : ", luas)
+
+luas_segitiga()
